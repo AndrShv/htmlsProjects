b47e470f6d7c2c0c408fd80e21e1c66de58a5423
BobikGavvv <andrey678a@gmail.com>
2024-01-25 18:06:35 +0200

    3

--- src/main/java/com/example/springboot/controllers/HelloController.java
+++ src/main/java/com/example/springboot/controllers/HelloController.java
@@ -1,14 +1,36 @@
 package com.example.springboot.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@RestController
+@RequestMapping("/api/calculator")
 public class HelloController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
+    @GetMapping("/add")
+    public double add(@RequestParam(name = "num1") double num1, @RequestParam(name = "num2")double num2) {
+        return num1 + num2;
     }
 
+    @GetMapping("/subtract")
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    @GetMapping("/multiply")
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    @GetMapping("/divide")
+    public double divide(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            return 0.0;
+        }
+    }
 }
+
